b27d8f541062589b36882f5b62c951d03786ef9b
dimacode <belovcode@gmail.com>
2025-05-06 18:18:52 +0300

    update user statistic table

--- src/components/AiInvestmens/userProfile.tsx
+++ src/components/AiInvestmens/userProfile.tsx
@@ -80,20 +80,20 @@ const ROIIcon = () => (
   </svg>
 );
 
-const DrawdownIcon = () => (
-  <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    <path d="M8 12L12 16L16 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    <path d="M12 8V16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-  </svg>
-);
+// const DrawdownIcon = () => (
+//   <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+//     <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+//     <path d="M8 12L12 16L16 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+//     <path d="M12 8V16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+//   </svg>
+// );
 
-const ProfitIcon = () => (
-  <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M12 2V22" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    <path d="M17 5H9.5C8.57174 5 7.6815 5.36875 7.02513 6.02513C6.36875 6.6815 6 7.57174 6 8.5C6 9.42826 6.36875 10.3185 7.02513 10.9749C7.6815 11.6313 8.57174 12 9.5 12H14.5C15.4283 12 16.3185 12.3687 16.9749 13.0251C17.6313 13.6815 18 14.5717 18 15.5C18 16.4283 17.6313 17.3185 16.9749 17.9749C16.3185 18.6313 15.4283 19 14.5 19H6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-  </svg>
-);
+// const ProfitIcon = () => (
+//   <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+//     <path d="M12 2V22" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+//     <path d="M17 5H9.5C8.57174 5 7.6815 5.36875 7.02513 6.02513C6.36875 6.6815 6 7.57174 6 8.5C6 9.42826 6.36875 10.3185 7.02513 10.9749C7.6815 11.6313 8.57174 12 9.5 12H14.5C15.4283 12 16.3185 12.3687 16.9749 13.0251C17.6313 13.6815 18 14.5717 18 15.5C18 16.4283 17.6313 17.3185 16.9749 17.9749C16.3185 18.6313 15.4283 19 14.5 19H6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+//   </svg>
+// );
 
 export default function UserProfile() {
   const [isDepositModalOpen, setIsDepositModalOpen] = useState(false);
@@ -252,26 +252,6 @@ export default function UserProfile() {
                     <td className="change-cell">
                       <span className="change-badge positive">+1.5%</span>
                     </td>
-                  </tr>
-                  <tr>
-                    <td className="icon-cell">
-                      <div className="stats-icon stats-icon-gray">
-                        <DrawdownIcon />
-                      </div>
-                    </td>
-                    <td>Max Drawdown</td>
-                    <td className="value-cell">{accountStats.maxDrawdown}</td>
-                    <td></td>
-                  </tr>
-                  <tr>
-                    <td className="icon-cell">
-                      <div className="stats-icon stats-icon-light-blue">
-                        <ProfitIcon />
-                      </div>
-                    </td>
-                    <td>Max Profit</td>
-                    <td className="value-cell">{accountStats.maxProfit}</td>
-                    <td></td>
                   </tr>
                 </tbody>
               </table>
